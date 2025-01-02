bf1bad162ab9869c3af57d9655cb105dd08dee0a
Karanyasuresh <143644155+Karanyasuresh@users.noreply.github.com>
2025-01-02 23:31:12 +0530

    Update LoginAutomationTest.java

--- src/main/java/com/automation/LoginAutomationTest.java
+++ src/main/java/com/automation/LoginAutomationTest.java
@@ -4,9 +4,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-//import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
 
-//import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LoginAutomationTest {
 
