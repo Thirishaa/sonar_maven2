399a9435d180cf5e7345909d36734774eb24d41d
Karanyasuresh <143644155+Karanyasuresh@users.noreply.github.com>
2025-01-02 23:15:11 +0530

    Update LoginAutomationTest.java

--- src/main/java/LoginAutomationTest.java
+++ src/main/java/LoginAutomationTest.java
@@ -10,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class LoginAutomationTest {
  public void testLogin() {
  // Set up the WebDriver
- System.setProperty("webdriver.chrome.driver", "C:\WebDriver");
+ System.setProperty("webdriver.chrome.driver", "C:\\webdriver\\chromedriver.exe");
  WebDriver driver = new ChromeDriver();
  try {
  // Navigate to the login page
