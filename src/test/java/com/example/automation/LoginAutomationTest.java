4e56cd1146c680e8bfb518b5fe153b779267512e
Karanyasuresh <143644155+Karanyasuresh@users.noreply.github.com>
2025-01-03 10:00:29 +0530

    Update LoginAutomationTest.java

--- src/test/java/com/example/automation/LoginAutomationTest.java
+++ src/test/java/com/example/automation/LoginAutomationTest.java
@@ -9,7 +9,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
+@SpringBootTest
 public class LoginAutomationTest {
 
     private WebDriver driver;
